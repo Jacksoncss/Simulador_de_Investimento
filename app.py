dfd049bb215178fc082e34f4035017fdb936533a
Murilo Lopes <rodrigues.murilo2006@gmail.com>
2025-05-22 21:55:07 -0300

    Corrigindo função simular e historico e implementando elementos da interface

--- app.py
+++ app.py
@@ -14,15 +14,33 @@ janela = ctk.CTk()
 janela.title("Simulador De Investimentos")
 janela.geometry("800x600")
 
-# ENTRADAS INVESTIMENTOS A- JACKSON
+
+entry_inicial = ctk.CTkEntry (janela, placeholder_text="Valor Inicial A (R$)") 
+entry_aporte = ctk.CTkEntry(janela, placeholder_text="Aporte Mensal A (R$)") 
+entry_taxa = ctk.CTkEntry(janela, placeholder_text="Taxa de Juros A (% ao mês)") 
+entry_inflacao = ctk.CTkEntry (janela, placeholder_text="Inflação A (% ao mês)") 
+entry_meses = ctk.CTkEntry (janela, placeholder_text="Duração A (meses)") 
+entry_inicial.pack(pady=2) 
+entry_aporte.pack(pady=2) 
+entry_taxa.pack(pady=2) 
+entry_inflacao.pack(pady=2) 
+entry_meses.pack(pady=2) 
+
+entry_inicial_b = ctk.CTkEntry (janela, placeholder_text="Valor Inicial B (R$)") 
+entry_aporte_b = ctk.CTkEntry (janela, placeholder_text="Aporte Mensal B (R$)") 
+entry_taxa_b = ctk.CTkEntry(janela, placeholder_text="Taxa de Juros B (% ao mês)") 
+entry_inflacao_b = ctk.CTkEntry (janela, placeholder_text="Inflação B (% ao mês)") 
+entry_meses_b = ctk.CTkEntry (janela, placeholder_text="Duração B (meses)") 
+entry_inicial_b.pack(pady=2) 
+entry_aporte_b.pack(pady=2) 
+entry_taxa_b.pack(pady=2) 
+entry_inflacao_b.pack(pady=2) 
+entry_meses_b.pack(pady=2) 
+
+resultado = ctk.CTkLabel(janela, text="", font=("Arial", 16)) 
+resultado.pack(pady=10)
 
 
-
-
-# ENTRADAS iNVESTIMENTOS B- JACKSON
-
-
-# Função de calculo do investimento
 def calcular_investimento(inicial, aporte, taxa, meses):
     montantes = []
     saldo = inicial
@@ -33,67 +51,68 @@ def calcular_investimento(inicial, aporte, taxa, meses):
         montantes.append(saldo)
     return montantes, saldo, total
 def simular():
-    valor_inicial = float(entry_inicial.get())  # type: ignore
-    aporte = float(entry_aporte.get()) # type: ignore
-    taxa = float(entry_taxa.get()) / 100 # type: ignore
-    meses = int(entry_meses.get()) # type: ignore
-    inflacao = float(entry_inflacao.get() or 0) / 100 # type: ignore
+    try:
+        valor_inicial = float(entry_inicial.get())  # type: ignore
+        aporte = float(entry_aporte.get()) # type: ignore
+        taxa = float(entry_taxa.get()) / 100 # type: ignore
+        meses = int(entry_meses.get()) # type: ignore
+        inflacao = float(entry_inflacao.get() or 0) / 100 # type: ignore
 
-    valor_inicial_b = float(entry_inicial_b.get()) # type: ignore
-    aporte_b = float(entry_aporte_b.get()) # type: ignore
-    taxa_b = float(entry_taxa_b.get()) / 100 # type: ignore
-    meses_b = int(entry_meses_b.get()) # type: ignore
-    inflacao_b = float(entry_inflacao_b.get() or 0) / 100 # type: ignore
+        valor_inicial_b = float(entry_inicial_b.get()) # type: ignore
+        aporte_b = float(entry_aporte_b.get()) # type: ignore
+        taxa_b = float(entry_taxa_b.get()) / 100 # type: ignore
+        meses_b = int(entry_meses_b.get()) # type: ignore
+        inflacao_b = float(entry_inflacao_b.get() or 0) / 100 # type: ignore
 
-    
-    montantes_a,final_a,investido_a = calcular_investimento(valor_inicial,aporte,taxa,meses)
-    montantes_b,final_b,investido_b = calcular_investimento(valor_inicial_b,aporte_b,taxa_b,meses_b)
-    valor_corrigido_a = final_a / ((1 + inflacao)** meses)   
-    valor_corrigido_b = final_b / ((1 + inflacao)** meses)
+        
+        montantes_a,final_a,investido_a = calcular_investimento(valor_inicial,aporte,taxa,meses)
+        montantes_b,final_b,investido_b = calcular_investimento(valor_inicial_b,aporte_b,taxa_b,meses_b)
+        valor_corrigido_a = final_a / ((1 + inflacao)** meses)   
+        valor_corrigido_b = final_b / ((1 + inflacao)** meses)
 
-    
-    historico.append({
-            "valor_final_a": final_a,
-            "valor_corrigido_a": valor_corrigido_a,
-            "total_investido_a": investido_a,
-            "meses_a": meses,
-            "taxa_a": taxa,
-            "valor_final_b": final_b,
-            "valor_corrigido_b": valor_corrigido_b,
-            "total_investido_b": investido_b,
-            "meses_b": meses_b,
-            "taxa_b": taxa_b
-        })
-    
-    resultado.configure(  # type: ignore
-        text= f"[A] Final: R$ {final_a:.2f}| Corrigido: R$ {valor_corrigido_a:.2f}\n"
-              f"[B] Final: R$ {final_b:.2f}| Corrigido: R$ {valor_corrigido_b:.2f}\n"
-    )
-    
-    for widget in frame_grafico.winfo_children():  # type: ignore
-        widget.destroy()
-    fig, ax = plt.subplots(figsize=(6, 3))
-    ax.plot(range(1, meses + 1), montantes_a, label="Investimento A", color="green")
-    ax.plot(range(1, meses_b + 1), montantes_b, label="Investimento B", color="blue")
-    ax.set_title("Evolução do Investimento")
-    ax.set_xlabel("Meses")
-    ax.set_ylabel("Valor (R$)")
-    ax.legend()
-    ax.grid(True)
+        
+        historico.append({
+                "valor_final_a": final_a,
+                "valor_corrigido_a": valor_corrigido_a,
+                "total_investido_a": investido_a,
+                "meses_a": meses,
+                "taxa_a": taxa,
+                "valor_final_b": final_b,
+                "valor_corrigido_b": valor_corrigido_b,
+                "total_investido_b": investido_b,
+                "meses_b": meses_b,
+                "taxa_b": taxa_b
+            })
+        
+        resultado.configure(  # type: ignore
+            text= f"[A] Final: R$ {final_a:.2f}| Corrigido: R$ {valor_corrigido_a:.2f}\n"
+                f"[B] Final: R$ {final_b:.2f}| Corrigido: R$ {valor_corrigido_b:.2f}\n"
+        )
+        
+        for widget in frame_grafico.winfo_children():  # type: ignore
+            widget.destroy()
+        fig, ax = plt.subplots(figsize=(6, 3))
+        ax.plot(range(1, meses + 1), montantes_a, label="Investimento A", color="green")
+        ax.plot(range(1, meses_b + 1), montantes_b, label="Investimento B", color="blue")
+        ax.set_title("Evolução do Investimento")
+        ax.set_xlabel("Meses")
+        ax.set_ylabel("Valor (R$)")
+        ax.legend()
+        ax.grid(True)
 
-    canvas = FigureCanvasTkAgg(fig, master=frame_grafico) # type: ignore
-    canvas.draw()
-    canvas.get_tk_widget().pack()
+        canvas = FigureCanvasTkAgg(fig, master=frame_grafico) # type: ignore
+        canvas.draw()
+        canvas.get_tk_widget().pack()
 
-except Exception as e:
-    resultado.configure(text=f"Erro: {str(e)}") # type: ignore
+    except Exception as e:
+            resultado.configure(text=f"Erro: {str(e)}") # type: ignore
 
-def mostrar_historico(historico):
+def mostrar_historico():
     texto = "\n".join([
-        f"{i+1}) [A] Final: R${h['valor_final_a']:.2f} | Corrigido: R${h['valor_corrigido_a']:.2f} | Meses: {h['meses_a']}\n"
-        f"    [B] Final: R${h['valor_final_b']:.2f} | Corrigido: R${h['valor_corrigido_b']:.2f} | Meses: {h['meses_b']}"
-        for i, h in enumerate(historico[-5:])
-    ])
+    f"{i+1}) [A] Final: R${h['valor_final_a']:.2f} | Corrigido: R${h['valor_corrigido_a']:.2f} | Meses: {h['meses_a']}\n"
+    f"    [B] Final: R${h['valor_final_b']:.2f} | Corrigido: R${h['valor_corrigido_b']:.2f} | Meses: {h['meses_b']}"
+    for i, h in enumerate(historico[-5:])
+])
 
     popup = ctk.CTkToplevel(janela)
     popup.geometry("400x300")
@@ -110,13 +129,15 @@ def exportar_csv():
             writer = csv.DictWriter(f,fieldnames=historico[0].keys())
             writer.writeheader()
             writer.writerows(historico)
-# Botoes - Jackson
 
+botao_simular = ctk.CTkButton(janela, text="Simular", command=simular) 
+botao_simular.pack(pady=10) 
+btn_historico = ctk.CTkButton(janela, text="Ver Histórico", command=mostrar_historico) 
+btn_historico.pack(pady=5) 
+btn_exportar = ctk.CTkButton(janela, text="Exportar CSV", command=exportar_csv) 
+btn_exportar.pack(pady=5)
 
+frame_grafico = ctk.CTkFrame (janela) 
+frame_grafico.pack(pady=10, fill="both", expand=True) 
 
-
-# GRAFICO - Jackson 
-
-
-
-# iniciar gui - Jackson
+janela.mainloop()
